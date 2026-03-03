af5508c0a05022e24552673555958ccdf3289cc7
Julian Sedano <ignovigenz@hotmail.com>
2026-03-03 01:22:05 +0000

    Db models corrections

--- src/models.py
+++ src/models.py
@@ -27,8 +27,9 @@ class Users(db.Model):
     subscription: Mapped[bool] = mapped_column(Boolean(), nullable=False)
     last_payment: Mapped[str] = mapped_column(nullable=False)
 
-    favorites: Mapped[List["Favorites"]] = relationship(back_populates="users")
-    learnmore: Mapped[List["LearnMore"]] = relationship(back_populates="users")
+    favoritecharacters: Mapped[List["FavoriteCharacters"]] = relationship(back_populates="characters")
+    favoriteplanets: Mapped[List["FavoritePlanets"]] = relationship(back_populates="planets")
+
 
 
     def serialize(self):
@@ -47,9 +48,9 @@ class Characters(db.Model):
     gender: Mapped[str] = mapped_column(nullable=False)
     hair_color: Mapped[str] = mapped_column(nullable=False)
     eyes_color: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
 
-    favorites: Mapped[List["Favorites"]] = relationship(back_populates="characters")
-    learnmore: Mapped[List["LearnMore"]] = relationship(back_populates="characters")
+    favoritecharacters: Mapped[List["FavoriteCharacters"]] = relationship(back_populates="characters")
 
     def serialize(self):
         return {
@@ -57,7 +58,8 @@ class Characters(db.Model):
             "name": self.name,
             "gender": self.gender,
             "hair_color": self.hair_color,
-            "eyes_color": self.eyes_color
+            "eyes_color": self.eyes_color,
+            "description": self.description
             # do not serialize the password, its a security breach
         }
 
@@ -67,99 +69,63 @@ class Planets(db.Model):
     name: Mapped[str] = mapped_column(nullable=False)
     population: Mapped[str] = mapped_column(nullable=False)
     terrain: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
     
-    favorites: Mapped[List["Favorites"]] = relationship(back_populates="planets")
-    learnmore: Mapped[List["LearnMore"]] = relationship(back_populates="planets")
+    favoriteplanets: Mapped[List["FavoritePlanets"]] = relationship(back_populates="planets")
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
             "population": self.population,
-            "terrain": self.terrain
-            # do not serialize the password, its a security breach
-        }
-    
-
-class Vehicles(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    model: Mapped[str] = mapped_column(nullable=False)
-    manufacturer: Mapped[str] = mapped_column(nullable=False)
-    
-    favorites: Mapped[List["Favorites"]] = relationship(back_populates="vehicles")
-    learnmore: Mapped[List["LearnMore"]] = relationship(back_populates="vehicles")
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "model": self.model,
-            "manufacturer": self.manufacturer
+            "terrain": self.terrain,
+            "description": self.description
             # do not serialize the password, its a security breach
         }
     
 
 
-class Favorites(db.Model):
+
+class FavoriteCharacters(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     id_user: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    type: Mapped[str] = mapped_column(nullable=False)
     id_characters: Mapped[int] = mapped_column(ForeignKey("characters.id"), nullable=False)
-    id_planets: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=False)
-    id_vehicles: Mapped[int] = mapped_column(ForeignKey("vehicles.id"), nullable=False)
     active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    users: Mapped["Users"] = relationship(back_populates="favorites")
+    users: Mapped["Users"] = relationship(back_populates="favoritecharacters")
 
     characters_id: Mapped[int] = mapped_column(ForeignKey("characters.id"))
-    characters: Mapped["Characters"] = relationship(back_populates="favorites")
-
-    planets_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
-    planets: Mapped["Planets"] = relationship(back_populates="favorites")
-
-    vehicles_id: Mapped[int] = mapped_column(ForeignKey("vehicles.id"))
-    vehicles: Mapped["Vehicles"] = relationship(back_populates="favorites")
+    characters: Mapped["Characters"] = relationship(back_populates="favoritecharacters")
 
     def serialize(self):
         return {
             "id": self.id,
             "id_user": self.id_user,
-            "type": self.type,
-            "id_characters": self.id_characters,
-            "id_planets": self.id_planets,
-            "id_vehicles": self.id_vehicles
+            "id_characters": self.id_characters
             # do not serialize the password, its a security breach
         }
     
 
-class LearnMore(db.Model):
+class FavoritePlanets(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    id_characters: Mapped[int] = mapped_column(ForeignKey("characters.id"), nullable=False)
+    id_user: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     id_planets: Mapped[int] = mapped_column(ForeignKey("planets.id"), nullable=False)
-    id_vehicles: Mapped[int] = mapped_column(ForeignKey("vehicles.id"), nullable=False)
-    description: Mapped[str] = mapped_column(nullable=False)
-
+    active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     users_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    users: Mapped["Users"] = relationship(back_populates="learnmore")
-
-    characters_id: Mapped[int] = mapped_column(ForeignKey("characters.id"))
-    characters: Mapped["Characters"] = relationship(back_populates="learnmore")
+    users: Mapped["Users"] = relationship(back_populates="favoriteplanets")
 
     planets_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
-    planets: Mapped["Planets"] = relationship(back_populates="learnmore")
+    planets: Mapped["Planets"] = relationship(back_populates="favoriteplanets")
 
-    vehicles_id: Mapped[int] = mapped_column(ForeignKey("vehicles.id"))
-    vehicles: Mapped["Vehicles"] = relationship(back_populates="learnmore")
 
     def serialize(self):
         return {
             "id": self.id,
-            "id_characters": self.id_characters,
-            "id_planets": self.id_planets,
-            "id_vehicles": self.id_vehicles,
-            "description": self.description
+            "id_user": self.id_user,
+            "id_planets": self.id_planets
             # do not serialize the password, its a security breach
         }
+
+
